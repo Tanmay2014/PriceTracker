2b38727f785ca55649a9369ef5d6c1012e282808
Tanmay <tanmaypetkar047@gmail.com>
2025-02-28 19:14:12 +0530

    Some changes that needs to be looked tomorrow

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import requests as r
 import bs4
 
-
 # todo
 # Make changes for amazon flipkart and Pricehistory
 
-
-
 base_url = 'https://www.amazon.in'
 # url = "https://www.amazon.in/dp/B0C8WP6B8R"
+# The Above comment was an attempt to search product using product id
 
-url = "https://www.amazon.in/BlueTM-SN580-NVMeTM-Internal-Storage/dp/B0C8WP6B8R?crid=3IXLKWAGWGTST&dib=eyJ2IjoiMSJ9.4bNezvwYcZmzlujVgvhQKX1s-LUf-_1uLtOhF7f5gMxStl7F5j_LkQJFD1Nud5HcAphmb9gOCeiuirjj369HWyWS6A2Nzl9LIcI0BfUIfZ46papM_qo4E2Md5PthLC6oqSdQGV0ftUuHft1Ldf_vpZ_RCpJdYS9KZ2alscpRD7YgEDtWVCgkm_yV0mU-yLpUEdqxKqwTcbW5geqyVjlnaMCPd7tC1NzfAIU6iGj35OA.B2amf1DX_oN1GUh9vqAncKp6VtnksV1sGY_rtyiRzJs&dib_tag=se&keywords=sn580&qid=1740550304&sprefix=sn580%2Caps%2C235&sr=8-1&th=1"
+url = "https://amzn.in/d/5w9fQZk"
+purl = "https://pricehistory.app/p/western-digital-wd-blue-sn580-pcie-gen-VWESmhw8"
 
 headers = {
     'user-agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0'
@@ -18,6 +17,10 @@ headers = {
 
 base_response = r.get(url, headers=headers)
 cookies = base_response.cookies
+
+def to_text(product_response):
+    soup = bs4.BeautifulSoup(product_response.text, features='lxml')
+    return soup
 
 def remove_extra_from_price(final_price):
     final_price = final_price.replace('<span class="a-price-whole">', '')
@@ -31,10 +34,20 @@ def remove_extra_from_prod_name(prod_name):
     prod_name = prod_name.replace('       </span>]','')
     return prod_name
 
+def ph_remove_extra_from_price(final_price):
+    final_price = final_price.replace('[<div class="col bg-info text-light"> <span class="label">Lowest:</span> <span class="amount">₹', '')
+    final_price = final_price.replace(' </span> </div>]', '')
+    a_final_price = int(final_price.replace(',',''))
+    return int(a_final_price)
+
+
 product_response = r.get(url, headers=headers, cookies=cookies)
-soup = bs4.BeautifulSoup(product_response.text, features='lxml')
-price_lines = soup.findAll(class_= "a-price-whole")
-prod_name = soup.findAll(class_="a-size-large product-title-word-break")
+price_lines = to_text(product_response).find_all(class_= "a-price-whole")
+prod_name = to_text(product_response).find_all(class_="a-size-large product-title-word-break")
 # print(type(prod_name), type(price_lines))
 final_price = str(price_lines[0])
-print(f"{remove_extra_from_prod_name(prod_name)} : {remove_extra_from_price(final_price)}")
+print(f"{remove_extra_from_prod_name(prod_name)[:30]} : {remove_extra_from_price(final_price)}")
+
+ph_response = r.get(purl, headers=headers, cookies=cookies)
+ph_price_lines = str(to_text(ph_response).find_all(class_="col bg-info text-light"))
+print(f"Lowest price for {remove_extra_from_prod_name(prod_name)[:30]} is {ph_remove_extra_from_price(ph_price_lines)}")
